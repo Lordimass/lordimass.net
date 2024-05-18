26b26d5ac66af06604f97aebec6d919091d29cc7
Lordimass <sam@tchrin.net>
2024-05-18 09:16:46 +0100

    Images added to grid instead of packing

--- js/art.js
+++ js/art.js
@@ -1,33 +1,9 @@
-const httpDirectory = "http://127.0.0.1:8080"
-var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-var last_refresh = new Date().getTime()
-
-resized_window = function() { // Reloads the images when the view width changes by enough
-  current = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-  var percentDifference = Math.abs(vw - current)/vw
-  if (percentDifference<0 && (new Date().getTime() - last_refresh) > 5000) {
-    // WILL NOT AUTO RESIZE, DISABLED FUNCTION FOR NOW BECAUSE IT WONT WORK, CHANGE LINE ABOVE TO RE-ENABLE, E.G. percentDifference > 0.1
-    vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    last_refresh = new Date().getTime()
-    place_images()
-    console.log("Viewwidth change, refreshing")
-  }
-
-
-
-}
-window.addEventListener("resize", resized_window)
+const httpDirectory = "http://localhost:8080"
+const artDirectory = "images/art"
 
 place_images = function() {
-  // Clear any existing files first
-  container = document.getElementById("image-container")
-  for (var img of container.children) {
-    console.log(`Removing ${img.getAttribute("src")}`)
-    img.remove()
-  }
-
-  // Send an HTTP request to the webserver to fetch image positions
-  fetch(URL = httpDirectory+`/?width=${vw}`) // Response is asyncronous
+  // Send an HTTP request to the webserver to fetch image paths
+  fetch(URL = httpDirectory+`/get_file_list?path=${artDirectory}`) // Response is asyncronous
       .then(response => {
           if (!response.ok) {
               throw new Error('Network response was not ok');
@@ -36,25 +12,17 @@ place_images = function() {
       })
 
       .then(data => {
-          positions = data;
-          console.log("Positions calculated by Rect-Packer code.");
+          console.log("Successfully fetched images");
+          console.log(data);
   
-          for (var key in positions) { // Iterate through each of the images
-              data = positions[key];
-
-              // Splitting the path into directory parts and taking just the file name
-              key = key.split("/")
-              key = key[key.length-1]
-              const path = "images/art/" + key
-
-              console.log(key + " => " + data);
+          for (var path_index in data) { // Iterate through each of the images
+              path = data[path_index]
+              console.log(path);
               const img = document.createElement("img");
               img.setAttribute("src", path);
-              img.setAttribute("width", data[2]);
+              img.setAttribute("width", "100%");
+              img.setAttribute("")
               img.classList.add("image")
-              img.style.position = "absolute";
-              img.style.left = data[0] + "px";
-              img.style.top = data[1] + "px";
               // <img src="images/art/Lordi Sticker.png" width="500" style="position: relative; transform:translate(50px, 50px)">
 
               const container = document.getElementById("image-container")
--- py/flask_app.py
+++ py/flask_app.py
@@ -2,35 +2,45 @@ from flask import Flask
 from flask import send_file, request
 from flask_cors import CORS, cross_origin
 from waitress import serve
-import importlib
-import sys
 import os
+import json
 
 from subroutines import *
-import rectpacker as rectpacker
 
 host = "0.0.0.0"
 port = 8080
-art_dir = "images/art"
+open_paths = [
+    "images/art"
+]
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/get_file_list", methods=['POST', 'GET'])
 @cross_origin()
-def hello_world():
-    string_width = request.args.get("width")
-    if string_width == None:
-        return {}
-    width = int(string_width)
-    tprint(width)
-    if "rectpacker" in sys.modules:
-        importlib.reload(rectpacker)
+def get_file_list():
+    path = request.args.get("path")
+    if path == None:
+        return "Please provide a path as an argument"
+    elif path not in open_paths:
+        return "You either do not have permission to access this path, or it isn't a valid path"
+    
+    
+    try: # Attempt to list the directory
+        directory_listing = os.listdir(path)
+    except FileNotFoundError: # User provided an invalid path
+        return f"No such file or directory: '{path}'"
+    
+    file_paths = []
+    for file in directory_listing:
+        file_paths.append(path + "/" + file)
 
-    rectpacker.RectPacker(width=width, dir = art_dir).pack(save=True)
-    tprint("Pack recieved, returning json file")
-    return send_file("positions.json")
+    print(file_paths)
+    fp = open("py/paths.json", "w")
+    json.dump(file_paths, fp, indent=4)
+    fp.close()
+
+    return send_file("paths.json")
 
 serve(app, host=host, port=port)
-print("Server Launched")
